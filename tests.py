8a7a0886d837cacfd084a6413c68c4bf08c8c4aa
Graham Gower <graham.gower@gmail.com>
2021-07-01 09:58:41 +0200

    Add subschema test.
    Closes #97.

--- tests.py
+++ tests.py
@@ -4,19 +4,26 @@ Test that all the examples are valid instances of the schema.
 Run with ``python3 -m pytest ``
 """
 import pathlib
-import json
+import functools
 
 import pytest
 import jsonschema
 from ruamel.yaml import YAML
+import hypothesis
+import hypothesis_jsonschema
+
+
+@functools.lru_cache(maxsize=None)
+def load_yaml(filename):
+    with open(filename) as f:
+        with YAML(typ="safe") as yaml:
+            data = yaml.load(f)
+    return data
 
 
 def validate(yaml_path):
-    yaml = YAML(typ="safe")
-    with open("demes-specification.yaml") as source:
-        schema = yaml.load(source)
-    with open(yaml_path) as source:
-        data = yaml.load(source)
+    schema = load_yaml("demes-specification.yaml")
+    data = load_yaml(yaml_path)
     jsonschema.validate(instance=data, schema=schema)
 
 
@@ -26,8 +33,21 @@ def validate(yaml_path):
 def test_examples(yaml_path):
     validate(yaml_path)
 
+
 @pytest.mark.parametrize(
     "yaml_path", map(str, pathlib.Path("test-cases/valid").glob("**/*.yaml"))
 )
 def test_test_cases_valid(yaml_path):
     validate(yaml_path)
+
+
+# Check that the MDM schema is a subschema of the HDM schema by generating
+# random data matching the MDM schema and validating against the HDM schema.
+@hypothesis.given(
+    mdm_data=hypothesis_jsonschema.from_schema(
+        load_yaml("demes-fully-qualified-specification.yaml")
+    )
+)
+def test_subschema(mdm_data):
+    hdm_schema = load_yaml("demes-specification.yaml")
+    jsonschema.validate(instance=mdm_data, schema=hdm_schema)
